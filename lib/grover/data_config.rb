c08c0e5c1a19bda829d27e8c091ba2616df07b27
Melvin Laguren <melvin.laguren@gmail.com>
2018-04-23 16:42:25 +0000

    Fixed tests

--- lib/grover/data_config.rb
+++ lib/grover/data_config.rb
@@ -1,22 +1,26 @@
 module Grover
   class DataConfig
+      
    def initialize(options = {})
        if options.empty?
            @location = "./configuration/grover.yml"
        else
            @location = options[:location]
        end
+       
+       @template = YAML.load_file("./configuration/grover.yml.template")
    end 
    
    def location
        @location
    end
    
-   def read_file
-       puts File.extname(@location)
+   def set_config
        if  File.extname(@location) != ".yml"
            return "#{@location} file extension is invalid"
        end
+       YAML.load_file(@location)
+       
    end
    
   end
--- spec/data_retrieval/data_config_spec.rb
+++ spec/data_retrieval/data_config_spec.rb
@@ -2,15 +2,20 @@ require './lib/grover/data_config'
 require 'yaml'
 
 RSpec.describe do
+  
+  before :all do
+    FileUtils.cp("./configuration/grover.yml.template", "./configuration/grover.yml")
+    FileUtils.cp("./configuration/grover.yml.template", "./configuration/custom.yml")
+    File.open("./configuration/test.json", "w") {|f| f.write("test:  name") }
+  end
+  
   describe "Grover::DataConfig" do
     it "should verify that the default configuration file exists" do
-      FileUtils.cp("./configuration/grover.yml.template", "./configuration/grover.yml")
       configuration = Grover::DataConfig.new
       expect(File.exists?(configuration.location)).to eq true
     end
     
     it "should verify that the custom configuration file exists" do
-      FileUtils.cp("./configuration/grover.yml.template", "./configuration/custom.yml")
       configuration = Grover::DataConfig.new( {"location":"./configuration/custom.yml"})
       expect(File.exists?(configuration.location)).to eq true
     end
@@ -21,10 +26,16 @@ RSpec.describe do
     end
     
     it "should return an error message if the configuration file is not a yaml file" do
-      File.open("./configuration/test.json", "w") {|f| f.write("test:  name") }
       configuration = Grover::DataConfig.new( {"location":"./configuration/test.json"})
-      expect(configuration.read_file).to eq "#{configuration.location} file extension is invalid"
+      expect(configuration.set_config).to eq "#{configuration.location} file extension is invalid"
     end
     
+    it "should contain data source setup information" do
+      configuration = Grover::DataConfig.new.set_config
+      expect(configuration).to have_key('SourceA')
+      expect(configuration).to have_key('SourceB')
+      expect(configuration['SourceA']).to have_key('type')
+      expect(configuration['SourceA']).to have_key('location')
+    end
   end
 end
