9c32e12e4fa8f333a2d634367297386eb7e1c7f4
Melvin Laguren <melvin.laguren@gmail.com>
2018-04-24 23:04:24 +0000

    Initial comparison logic

--- spec/basic_comparison_spec.rb
+++ spec/basic_comparison_spec.rb
@@ -8,6 +8,11 @@ RSpec.describe do
        
        it "should return a mismatch message if both sources do not have the same number of rows" do
            expect(Grover::BasicComparisons.new('./test_data/results.json','./test_data/rows_mismatch.json').equal_rows?).to eq false
+           
+       end
+       
+       it "should return nothing" do
+           expect(Grover::BasicComparisons.new('./test_data/results.json','./test_data/rows_mismatch.json').source_delta).to eq false
        end
        
    end
