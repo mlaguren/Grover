2587f0b4dcc3c364996b4a8ea52a52921de8e271
Melvin Laguren <melvin.laguren@gmail.com>
2018-04-24 06:44:09 +0000

    Temporary Folder spec

--- lib/grover/data_conversion.rb
+++ lib/grover/data_conversion.rb
@@ -11,9 +11,16 @@ module Grover
         CSV.foreach(@file, headers: true) do |row|
           data << row.to_hash
         end
-        return data
+          return data
         end
       end
       
+      def to_json_file(epoch)
+        Dir.mkdir('tmp') unless File.exists?('tmp')
+        Dir.mkdir("tmp/#{epoch.to_s}") unless File.exists?("data/#{epoch.to_s}")
+        File.open("tmp/#{epoch.to_s}/#{File.basename(@file, ".*")}.json","w") do |f|
+            f.write(JSON.pretty_generate(self.to_hash))
+        end
+      end
   end
 end
--- spec/data_conversion/data_conversion_spec.rb
+++ spec/data_conversion/data_conversion_spec.rb
@@ -5,12 +5,17 @@ require 'csv'
 
 RSpec.describe do
     
-      describe "Grover::DataInit" do
+      describe "Grover::DataConversion" do
           
         it "should convert the file to an array of hashes" do
             expect(Grover::DataConversion.new("./test_data/test.csv").to_hash.class).to eq Array
             expect(Grover::DataConversion.new("./test_data/test.csv").to_hash[0].class).to eq Hash
-
+        end
+        
+        it "should create a json file in a temporary folder" do
+            epoch = Time.now.to_i
+            Grover::DataConversion.new("./test_data/test.csv").to_json_file(epoch)
+            expect(File.exists?("./tmp/#{epoch}/test.json")).to eq true
         end
           
       end
