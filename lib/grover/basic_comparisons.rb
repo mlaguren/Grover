c7fca3efe1649cddcc03557c6ed13164292a0235
Melvin Laguren <melvin.laguren@gmail.com>
2018-05-09 05:52:11 +0000

    Adding missing data method and specs

--- lib/grover/basic_comparisons.rb
+++ lib/grover/basic_comparisons.rb
@@ -1,6 +1,5 @@
 module Grover
     class BasicComparisons
-        
         def initialize(source_a, source_b, options = {})
             @source_a = JSON.parse(File.read(source_a))
             @source_b = JSON.parse(File.read(source_b))
@@ -13,6 +12,10 @@ module Grover
             else 
                 false
             end
+        end
+        
+        def does_not_exists
+            [ (@source_a - @source_b), (@source_b - @source_a) ]
         end
         
     end
--- spec/basic_comparison_spec.rb
+++ spec/basic_comparison_spec.rb
@@ -1,7 +1,7 @@
 require './lib/grover/basic_comparisons'
 
 RSpec.describe do 
-   describe "Grover::BasicComparisons" do
+   describe "Grover::BasicComparison" do
        it "should return success message if both sources have the same number of rows" do
            expect(Grover::BasicComparisons.new('./test_data/results.json','./test_data/rows_match.json').equal_rows?).to eq true
        end
@@ -11,9 +11,17 @@ RSpec.describe do
            
        end
        
-       it "should return nothing" do
-           expect(Grover::BasicComparisons.new('./test_data/results.json','./test_data/rows_mismatch.json').source_delta).to eq false
+       it "should return an empty array when the data matches" do
+           expect(Grover::BasicComparisons.new('./test_data/results.json','./test_data/results.json').does_not_exists).to eq [[], []]
        end
+
+       it "should the row that's in A that is not in B" do
+           expect(Grover::BasicComparisons.new('./test_data/results.json','./test_data/rows_mismatch.json').does_not_exists).to eq [[{"id"=>"25","first_name"=>"Courtnay","last_name"=>"Caslake","email"=>"ccaslakeo@tumblr.com","gender"=>"Male","ip_address"=>"48.49.120.107"}],[]]
+       end       
+       
+       it "should the row that's in B that is not in A" do
+           expect(Grover::BasicComparisons.new('./test_data/rows_mismatch.json', './test_data/results.json').does_not_exists).to eq [[],[{"id"=>"25","first_name"=>"Courtnay","last_name"=>"Caslake","email"=>"ccaslakeo@tumblr.com","gender"=>"Male","ip_address"=>"48.49.120.107"}]]
+       end  
        
    end
 end
